4d1daa0af813e46b62fa63ddda4f4ba104f990dc
xclusive <ya.5164646777752015@yandex.ru>
2017-03-08 13:31:54 +0200

    Use PUBLIC const for static resources url

--- Blog/views/_layout/header.php
+++ Blog/views/_layout/header.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="<?=\Framework\Core\Config::APP_ROOT;?>/public/styles/bootstrap.min.css">
-    <script src="<?=\Framework\Core\Config::APP_ROOT;?>/public/js/jquery-3.1.1.min.js"></script>
-    <script src="<?=\Framework\Core\Config::APP_ROOT;?>/public/js/bootstrap.min.js"></script>
-    <script src="<?=\Framework\Core\Config::APP_ROOT;?>/public/js/scripts.js"></script>
+    <link rel="stylesheet" href="<?=\Framework\Core\Config::PUBLIC;?>/styles/bootstrap.min.css">
+    <script src="<?=\Framework\Core\Config::PUBLIC;?>/js/jquery-3.1.1.min.js"></script>
+    <script src="<?=\Framework\Core\Config::PUBLIC;?>/js/bootstrap.min.js"></script>
+    <script src="<?=\Framework\Core\Config::PUBLIC;?>/js/scripts.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
