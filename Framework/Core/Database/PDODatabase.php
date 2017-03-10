67edf80a07373b0f0ba26147ce30e80496479718
xclusive <ya.5164646777752015@yandex.ru>
2017-03-10 11:27:27 +0200

    Fix for utf8

--- Framework/Core/Database/PDODatabase.php
+++ Framework/Core/Database/PDODatabase.php
@@ -10,7 +10,12 @@ class PDODatabase implements DatabaseInterface
 
     public function __construct(string $host, string $dbName, string $user, string $password)
     {
-        $this->db = new \PDO("mysql:host={$host};dbname={$dbName};charset=utf8", $user, $password);
+        $this->db = new \PDO(
+            "mysql:host={$host};dbname={$dbName};charset=utf8",
+            $user,
+            $password,
+            [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
+        );
     }
 
     public function prepare(string $query): DatabaseStatementInterface
