57eb46d43ffb35943bc54fe49efaa4990a69f347
xclusive <ya.5164646777752015@yandex.ru>
2017-03-11 11:06:36 +0200

    Update dump.sql + diagram

--- .db_export/dump.sql
+++ .db_export/dump.sql
@@ -7,6 +7,7 @@
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `authorId` int(10) unsigned DEFAULT NULL,
+  `guestId` int(10) unsigned DEFAULT NULL,
   `postId` int(10) unsigned NOT NULL,
   `body` text NOT NULL,
   `createdOn` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -15,6 +16,8 @@ CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (`id`),
   KEY `FK_comments_users` (`authorId`),
   KEY `FK_comments_posts` (`postId`),
+  KEY `FK_comments_guests` (`guestId`),
+  CONSTRAINT `FK_comments_guests` FOREIGN KEY (`guestId`) REFERENCES `guests` (`id`),
   CONSTRAINT `FK_comments_posts` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`),
   CONSTRAINT `FK_comments_users` FOREIGN KEY (`authorId`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -22,6 +25,18 @@ CREATE TABLE IF NOT EXISTS `comments` (
 DELETE FROM `comments`;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+
+CREATE TABLE IF NOT EXISTS `guests` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `ip` binary(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DELETE FROM `guests`;
+/*!40000 ALTER TABLE `guests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `guests` ENABLE KEYS */;
 
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -34,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   PRIMARY KEY (`id`),
   KEY `FK__users` (`authorId`),
   CONSTRAINT `FK__users` FOREIGN KEY (`authorId`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 
 DELETE FROM `posts`;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
@@ -43,14 +58,14 @@ INSERT INTO `posts` (`id`, `authorId`, `title`, `body`, `createdOn`, `updatedOn`
 	(4, 10, 'Dependency injection', 'In software engineering, dependency injection is a technique whereby one object supplies the dependencies of another object. A dependency is an object that can be used (a service). An injection is the passing of a dependency to a dependent object (a client) that would use it. The service is made part of the client\'s state.[1] Passing the service to the client, rather than allowing a client to build or find the service, is the fundamental requirement of the pattern.', '2017-03-09 21:57:04', '2017-03-10 08:17:44', NULL),
 	(5, 10, 'Какво е Рекурсия', 'В програмирането с рекурсия се обозначава случай, когато една подпрограма вика предходна своя функция. Рекурсията условно се разделя в две категории: директна (пряка) и индиректна (косвена). Рекурсията е пряка, когато в тялото на подпрограмата има референция към нея. Косвена е тази рекурсия, при която една подпрограма вика друга, а тя вика предходната. Съществуват и случаи на косвена рекурсия, при които подпрограмата извиква себе си, след поредица от обръщения към други подпрограми.', '2017-03-10 08:18:44', '2017-03-10 08:18:44', NULL),
 	(7, 10, 'Lazy loading pattern', 'Lazy loading is a design pattern commonly used in computer programming to defer initialization of an object until the point at which it is needed. It can contribute to efficiency in the program\'s operation if properly and appropriately used. The opposite of lazy loading is eager loading.', '2017-03-10 08:19:34', '2017-03-10 08:19:34', NULL),
-	(8, 10, 'Конструктор (обектно-ореинтирано програмиране)', 'В обектно-ориентираното програмиране конструкторът е блок от инструкции, който се изпълнява за инициализация на един обект при неговото създаване. Практически конструкторът може да извършва всякакъв вид действия, но ако не друго, то е поне признак за много лош стил на програмиране да се изполва за действия, които не засягат пряко създавания обект, тъй като това може да доведе до неочаквани грешки, особено в големи програми. В езика за програмиране C++ конструкторът се представя като член-функция на един обект, носеща същото име като него. Тя и деструкторът са единствените типове дефиниции на функции, за които не се дефинира тип на връщания резултат. В тази статия примерите са дадени на този език.', '2017-03-10 08:20:03', '2017-03-10 08:20:09', NULL),
+	(8, 10, 'Конструктор (обектно-ореинтирано програмиране) d', 'a В обектно-ориентираното програмиране конструкторът е блок от инструкции, който се изпълнява за инициализация на един обект при неговото създаване. Практически  dконструкторът може да извършва всякакъв вид действия, но ако не друго, то е поне признак за много лош стил на програмиране да се изполва за действия, които не засягат пряко създавания обект, тъй като това може да доведе до неочаквани грешки, особено в големи програми. В езика за програмиране C++ конструкторът се представя като член-функция на един обект, носеща същото име като него. Тя и деструкторът са единствените типове дефиниции на функции, за които не се дефинира тип на връщания резултат. В тази статия примерите са дадени на този език.', '2017-03-10 08:20:03', '2017-03-10 17:55:42', NULL),
 	(9, 10, 'Парадигма на програмиране', 'Парадигмата на програмиране, парадигма за програмиране или програмна парадигма представлява фундаменталния стил на програмиране. Има множество програмни парадигми, но основните сред тях са: обектно-ориентирано, императивно, функционално и декларативно [1].', '2017-03-10 08:20:41', '2017-03-10 08:20:46', NULL),
 	(10, 10, 'Graph (discrete mathematics)', 'In mathematics, and more specifically in graph theory, a graph is a structure amounting to a set of objects in which some pairs of the objects are in some sense "related". The objects correspond to mathematical abstractions called vertices (also called nodes or points) and each of the related pairs of vertices is called an edge (also called an arc or line).[1] Typically, a graph is depicted in diagrammatic form as a set of dots for the vertices, joined by lines or curves for the edges. Graphs are one of the objects of study in discrete mathematics.', '2017-03-10 08:21:19', '2017-03-10 08:21:24', NULL),
 	(11, 10, 'Depth-first search (DFS)', 'Depth-first search (DFS) is an algorithm for traversing or searching tree or graph data structures. One starts at the root (selecting some arbitrary node as the root in the case of a graph) and explores as far as possible along each branch before backtracking.', '2017-03-10 08:22:01', '2017-03-10 08:22:05', NULL),
 	(13, 10, 'Breadth-first search (BFS)', 'Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures. It starts at the tree root (or some arbitrary node of a graph, sometimes referred to as a \'search key\'[1]) and explores the neighbor nodes first, before moving to the next level neighbors.', '2017-03-10 08:22:39', '2017-03-10 08:22:39', NULL),
-	(14, 10, 'Едсхер Дейкстра', 'Едсхер Дейкстра (понякога неправилно изписвано Дийкстра; на нидерландски: Edsger Wybe Dijkstra, нидерландско произношение: [ˈɛtsxər ˈwibə ˈdɛɪkstra]) е нидерландски информатик.', '2017-03-10 08:23:16', '2017-03-10 08:23:21', NULL),
+	(14, 10, 'Едсхер Дейкстра', 'Едсхер Дейкстра (понякога неправилно изписвано Дийкстра; на нидерландски: Edsger Wybe Dijkstra, нидерландско произношение: [ˈɛtsxər ˈwibə ˈdɛɪkstra]) е нидерландски информатик. През 1972 г. получава наградата „А. М. Тюринг“ за основополагащите си приноси за развитие на езиците за програмиране. Бил е Шлумбергер сентениял професор по компютърни науки в Тексаския университет в Остин от 1984 до 2000 г.[1]\r\n\r\nПрез 2003 г. наградата ACM PODC за влиятелини научни публикации върху принципите на разпределените изчислителни системи е преименувана на Премия Дейкстра.[1]\r\n\r\nПръв въвежда термина структурно програмиране в информатиката. Автор на едноименния алгоритъм.[1]', '2017-03-10 08:23:16', '2017-03-10 14:44:26', NULL),
 	(15, 10, 'Dijkstra\'s algorithm', 'Dijkstra\'s algorithm is an algorithm for finding the shortest paths between nodes in a graph, which may represent, for example, road networks. It was conceived by computer scientist Edsger W. Dijkstra in 1956 and published three years later.[1][2]', '2017-03-10 08:23:47', '2017-03-10 08:23:47', NULL),
-	(17, 10, 'Bogosort Algorithm', 'In computer science, bogosort[1][2] (also permutation sort, stupid sort,[3] slowsort,[4] shotgun sort or monkey sort) is a highly ineffective sorting algorithm based on the generate and test paradigm. The algorithm successively generates permutations of its input until it finds one that is sorted. It is not useful for sorting, but may be used for educational purposes, to contrast it with more efficient algorithms.', '2017-03-10 08:25:00', '2017-03-10 08:25:00', NULL);
+	(17, 10, 'Bogosort Algorithm', 'In computer science, bogosort[1][2] (also permutation sort, stupid sort,[3] slowsort,[4] shotgun sort or monkey sort) is a highly ineffective sorting algorithm based on the generate and test paradigm. The algorithm successively generates permutations of its input until it finds one that is sorted. It is not useful for sorting, but may be used for educational purposes, to contrast it with more efficient algorithms. .', '2017-03-10 08:25:00', '2017-03-11 09:05:04', NULL);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 
 CREATE TABLE IF NOT EXISTS `post_tags` (
@@ -60,7 +75,7 @@ CREATE TABLE IF NOT EXISTS `post_tags` (
   PRIMARY KEY (`id`),
   KEY `FK_post_tags_posts` (`postId`),
   CONSTRAINT `FK_post_tags_posts` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 
 DELETE FROM `post_tags`;
 /*!40000 ALTER TABLE `post_tags` DISABLE KEYS */;
@@ -86,7 +101,8 @@ CREATE TABLE IF NOT EXISTS `templates` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `cssFile` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 DELETE FROM `templates`;
@@ -99,32 +115,37 @@ INSERT INTO `templates` (`id`, `name`, `cssFile`) VALUES
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL DEFAULT '0',
-  `password` varchar(255) NOT NULL DEFAULT '0',
-  `roleId` int(11) unsigned NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `roleId` int(11) unsigned NOT NULL DEFAULT '2',
   `templateId` int(11) unsigned NOT NULL DEFAULT '1',
   `createdOn` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedOn` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
   KEY `FK_users_user_roles` (`roleId`),
   KEY `FK_users_templates` (`templateId`),
   CONSTRAINT `FK_users_templates` FOREIGN KEY (`templateId`) REFERENCES `templates` (`id`),
   CONSTRAINT `FK_users_user_roles` FOREIGN KEY (`roleId`) REFERENCES `user_roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 DELETE FROM `users`;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `username`, `password`, `roleId`, `templateId`, `createdOn`, `updatedOn`) VALUES
-	(9, 'videlin', '$2y$10$rp7f6rWK3OBYKxER32QIyO.qruIgQjW/mK/tF1gA7WYOnd3wCd2XG', 2, 1, '2017-03-05 19:47:53', '2017-03-10 10:45:04'),
-	(10, 'admin', '$2y$10$tViY3e4wQxfYAopgiAjcM.uxFKd42LVqWPabp7zfoADIKXzzOWzHK', 1, 2, '2017-03-06 11:55:20', '2017-03-10 11:41:46'),
-	(11, 'test', '$2y$10$qxk4Mhc535BFFQFhE8VjnONH17gyXwF9pQpTQqShXixShqTgBXNn.', 2, 2, '2017-03-10 10:45:20', '2017-03-10 10:57:27');
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `roleId`, `templateId`, `createdOn`, `updatedOn`) VALUES
+	(9, 'videlin', '$2y$10$rp7f6rWK3OBYKxER32QIyO.qruIgQjW/mK/tF1gA7WYOnd3wCd2XG', 'Виделин', 'videlin@gmail.com', 2, 2, '2017-03-05 19:47:53', '2017-03-11 10:18:04'),
+	(10, 'admin', '$2y$10$tViY3e4wQxfYAopgiAjcM.uxFKd42LVqWPabp7zfoADIKXzzOWzHK', 'SysOP', 'admin@blog.dev', 1, 1, '2017-03-06 11:55:20', '2017-03-11 10:18:11'),
+	(11, 'test', '$2y$10$qxk4Mhc535BFFQFhE8VjnONH17gyXwF9pQpTQqShXixShqTgBXNn.', 'Божинов', 'test@dir.nik', 2, 2, '2017-03-10 10:45:20', '2017-03-11 10:18:20'),
+	(12, 'anotherTest', '$2y$10$vgNt2Q5LRoaSsfwS4eeP7ODzeqyHJ9tFPtgMnP7GgupT51LqmbS8S', 'Наско', 'anotherTest@abv.bg', 2, 1, '2017-03-11 10:57:37', '2017-03-11 11:02:23');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 CREATE TABLE IF NOT EXISTS `user_roles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 DELETE FROM `user_roles`;
